f64bbd65506149cc9b421825477c915d63f671f2
marknjoroge <marknnjorogeh@gmail.com>
2024-01-04 18:56:53 +0300

    plays notification when jobs are found

--- content.js
+++ content.js
@@ -1,11 +1,25 @@
+var playing = false;
+
 function checkAndRefresh() {
     var element = document.getElementsByClassName("l3t8Qe")[0];
 
-    if (element && element.textContent.trim() === "0") {
-        console.log("Refreshing page...");
-        location.reload();
+    if (element && !playing) {
+        if (element.textContent.trim() === "0") {
+            console.log("Refreshing page...");
+            location.reload();
+        } else {
+            playNotification();
+            return;
+        }
     }
 }
 
-setInterval(checkAndRefresh, 10000);
+function playNotification() {
+    var myAudio = new Audio(chrome.runtime.getURL("assets/music/alarm.mp3"));
+    if (!playing) { myAudio.play(); playing = true; }
+}
 
+setInterval(checkAndRefresh, 10000);
+// document.body.addEventListener("mousemove", function () {
+//     playNotification();
+// })
