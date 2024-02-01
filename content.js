cab016c9d64ffcc5ae72e8ce92963c30505c22d0
marknjoroge <marknnjorogeh@gmail.com>
2024-02-01 09:44:06 +0300

    spinner support added

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ var playing = false;
 
 function checkAndRefresh() {
     // var element = document.querySelectorAll('.leading-6.py-2.text-neutral-800.w-1/3.text-center');
-    // var spinner = document.querySelectorAll('.jsx-3750104642.spinner-wrapper');
+    var spinner = document.querySelectorAll('.jsx-3750104642.spinner-blade');
     // var title
 
     var pageText = document.body.textContent || document.body.innerText;
@@ -10,10 +10,10 @@ function checkAndRefresh() {
     if (pageText.includes("Your task queue is currently empty")) {
         location.reload();
     } else {
-        // if (!spinner) {
-        playNotification();
-        return;
-        // }
+        if (!spinner) {
+            playNotification();
+            return;
+        }
     }
 
 
